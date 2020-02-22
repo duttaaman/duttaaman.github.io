4ff6c083ff0ff0f7389b8fca1aa44b82b545c862
Aman Dutta <aman.dutta@ad.infosys.com>
2020-02-23 04:16:12 +0530

    Compare option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CountryFilterComponent } from './country-filter/country-filter.component';
+import { CountryFilterComponent, DialogBox } from './country-filter/country-filter.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpRequestService } from './country-filter/country-filter.service';
@@ -11,11 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import {MatAutocompleteModule} from '@angular/material';
 import { MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
-
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    CountryFilterComponent
+    CountryFilterComponent,
+    DialogBox,
+   
   ],
   imports: [
     BrowserModule,
@@ -26,10 +29,13 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     //  MatAutocompleteModule
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   providers: [HttpRequestService],
-  exports: [MatAutocompleteModule, MatInputModule],
+  entryComponents:[DialogBox],
+  // exports: [MatAutocompleteModule, MatInputModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/country-filter/country-filter.component.ts
+++ src/app/country-filter/country-filter.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {HttpRequestService} from './country-filter.service';
-import {map, startWith} from 'rxjs/operators';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { HttpRequestService } from './country-filter.service';
+import { map, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+}
+
 @Component({
   selector: 'app-country-filter',
   templateUrl: './country-filter.component.html',
@@ -10,56 +16,109 @@ import { Observable } from 'rxjs';
 })
 export class CountryFilterComponent implements OnInit {
 
- 
+
   options: string[] = ['Delhi', 'Mumbai', 'Banglore'];
-  countries:string[]=[];
+  countries: string[] = [];
   myControl = new FormControl();
   filteredOptions: Observable<string[]>;
-  countryName='';
+  countryName = '';
+  secondCountry ='';
   selected;
   selected1;
-  key;
-  key1;
-  constructor( private httpService:HttpRequestService) { }
+  // key;
+  // key1;
+  names = []
+  operation: any;
+  flag = false;
+  constructor(private httpService: HttpRequestService, public dialog: MatDialog) { }
 
   ngOnInit() {
 
     //Observables observes the text in input field
 
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-);
+    // this.filteredOptions = this.myControl.valueChanges.pipe(
+    //   startWith(''),
+    //   map(value => this._filter(value))
+    // );
 
-    this.httpService.getAll().subscribe(data =>{
+  }
+
+  InitialFetch(value) {
+    this.countries = [];
+    this.httpService.getCountryName(value).subscribe(data => {
       for (const cust in data) {
         this.countries.push(data[cust].name);
       }
-    });
-  
-  }
-
-  countryChange(value){
-    
-    this.httpService.getCountryDetails(value).subscribe(data =>{
-      this.selected=data[0];
-      // alert(JSON.stringify(this.selected));
-      this.countryName=value;
-      this.key=Object.keys(this.selected);
     })
   }
 
-  compareCountries(value){
-    this.httpService.getCountryDetails(value).subscribe(data =>{
-      this.selected=data[0];
-      this.key1=Object.keys(this.selected);
+  countryChange(value) {
+    this.operation = "";
+
+    this.flag = true;
+    this.httpService.getCountryDetails(value).subscribe(data => {
+      this.selected = data[0];
+
+      this.countryName = value;
+      // this.key = Object.keys(this.selected);
     })
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+  compareCountries(value) {
+      this.httpService.getCountryDetails(value).subscribe(data => {
+        this.selected1 = data[0];
+        this.secondCountry=value;
+        // this.key1 = Object.keys(this.selected1);
+      })
 
-    return this.countries.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+
+  }
+
+  // private _filter(value: string): string[] {
+  //   const filterValue = value.toLowerCase();
+
+  //   return this.countries.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  // }
+
+  openDialog(country: string): void {
+    if (this.flag && this.countryName != country) {
+      const dialogRef = this.dialog.open(DialogBox, {
+        width: '250px',
+        data: { name: country }
+
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.operation = result;
+
+        if (this.operation == "compare")
+          this.compareCountries(country);
+        else if (this.operation == "overwrite")
+          this.countryChange(country)
+      });
+    }
+    else
+      this.countryChange(country)
+
+  }
+
 }
+
+
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: 'dialog-box.html',
+})
+export class DialogBox {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogBox>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
